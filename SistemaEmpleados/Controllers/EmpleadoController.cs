d669d5d1efed7217b2061cf2f5b665b3ac6dbebb
Neiry Rachel B.W <rachel.benua@gmail.com>
2026-07-07 16:15:33 -0400

    Add project files.

---
+++ SistemaEmpleados/Controllers/EmpleadoController.cs
@@ -0,0 +1,56 @@
+﻿using System.Numerics;
+using System.Reflection.Metadata.Ecma335;
+using Microsoft.AspNetCore.Mvc;
+using SistemaEmpleados.Models;
+
+namespace SistemaEmpleados.Controllers;
+
+public class EmpleadoController : Controller
+{
+    public IActionResult Index()
+    {
+        //Lista hardcodeada - sin base de datos
+        var lista = new List<Empleado>
+        {
+            new Empleado
+            {
+                Id = 1,
+                Nombre="Jeifferson David",
+                Departamento="RRHH",
+                Salario=35000m,
+                Activo=true
+            },
+
+            new Empleado
+            {
+            Id = 2,
+            Nombre = "Joseidy Puello",
+            Departamento = "Tecnologia",
+            Salario=42000m,
+            Activo=true
+            },
+
+            new Empleado
+            {
+                Id = 3,
+                Nombre="Angel Natera",
+                Departamento="Finanzas",
+                Salario=38000m,
+                Activo=true
+            },
+
+            new Empleado
+            {
+                Id = 4,
+                Nombre="Pedro Gomez",
+                Departamento="Operaciones",
+                Salario=28000m,
+                Activo=false
+            },  
+
+        };
+        return View(lista); //envía la lista completa a la View
+
+    }
+
+}
