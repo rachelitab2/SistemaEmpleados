80ddd6ff7890f53edceeae0022ed5b7df3de43ce
Neiry Rachel B.W <rachel.benua@gmail.com>
2026-07-07 23:00:47 -0400

    Solucion de Varios Errores y punto --9 realizado

--- SistemaEmpleados/Program.cs
+++ SistemaEmpleados/Program.cs
@@ -4,6 +4,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddControllersWithViews();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();           
+       
 
 var app = builder.Build();
 
