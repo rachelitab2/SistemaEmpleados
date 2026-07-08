4a192d8d76e329b43b9ec9d5ce0255c749e672eb
Neiry Rachel B.W <rachel.benua@gmail.com>
2026-07-07 23:46:01 -0400

    Completado, practica finalizada

--- SistemaEmpleados/Program.cs
+++ SistemaEmpleados/Program.cs
@@ -1,20 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();           
-       
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -23,12 +17,12 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.MapBlazorHub(); 
+app.MapRazorPages();
+app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+
 app.Run();
